30f6e0f4ed07dbca286bf7aa7e75df2258ffdeac
Bohdan <fomenchukbohdan@gmail.com>
2023-09-26 21:56:52 +0300

    Complete U.S. States Game

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -2,24 +2,37 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+guess = turtle.Turtle()
+
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+correct_number = 0
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name")
 data = pandas.read_csv("50_states.csv")
-states = data.to_dict()
-print(states)
+states = data.state.to_list()
+x_cor = data.x.to_list()
+y_cor = data.y.to_list()
+guessed_states = []
 
-# if answer_state in states:
-#     turtle.goto()
-#     turtle.write(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{correct_number}/50 States Correct",
+                                    prompt="What's another state's name").title()
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        states_to_learn = pandas.DataFrame(missing_states)
+        states_to_learn.to_csv("states_to_learn")
+        break
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        state_index = states.index(answer_state)
+        guess.penup()
+        guess.hideturtle()
+        guess.goto(x_cor[state_index], y_cor[state_index])
+        guess.write(answer_state)
+        correct_number += 1
 
-
-def get_mouse_click_color(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_color)
-turtle.mainloop()
