2bf806d72f92db5fe4815aa90476673f2bbd33b5
Bohdan <fomenchukbohdan@gmail.com>
2023-10-22 16:10:58 +0300

    First version

---
+++ tkinter/playground.py
@@ -0,0 +1,26 @@
+def add(*args):
+    print(args[0])
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+#(add(3, 4, 5, 7, 8, 9))
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    return print(n)
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan", model="GT-R")
